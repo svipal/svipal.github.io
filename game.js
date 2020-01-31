e47294faeadb5eec5d6ce9113929543ba5457e6b
Svipal <svipal@tuta.io>
2020-01-31 17:13:41 +0100

    keeping up with the work on the fork, added dynamic layer test. Everything works except, mysteriously, a portion of the layer disappearing.

--- game.js
+++ game.js
@@ -23,24 +23,73 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+var map;  
+var tileset;
+var layer;
+var controls;
+
 function preload() { 
   this.load.image('background', 'assets/images/background.png');
   this.load.image('tiles', 'assets/tilesets/newtileset.png');
   // Load the export Tiled JSON
   this.load.tilemapTiledJSON('map', 'assets/tilemaps/testfinal.json'); }
 
+
+
+
 function create() { 
   const backgroundImage = this.add.image(0, 0,'background').setOrigin(0.5, 0);
   backgroundImage.setScale(2, 0.8);
  
-  const map = this.make.tilemap({ key: 'map' });
-  const tileset = map.addTilesetImage('newtileset', 'tiles');
-  const platforms = map.createStaticLayer('Calque 1', tileset, 0, 200);
+  map = this.make.tilemap({ key: 'map' });
+  tileset = map.addTilesetImage('newtileset', 'tiles');
+  const layer = map.createDynamicLayer('Calque 1', tileset, 0, 200);
 
-  
+  marker = this.add.graphics();
+  marker.lineStyle(2, 0x000000, 1);
+  marker.strokeRect(0, 0, map.tileWidth, map.tileHeight);
+
   this.cameras.main.centerOn(0,config.height/2)
 
-  
+  var cursors = this.input.keyboard.createCursorKeys();
+  var controlConfig = {
+      camera: this.cameras.main,
+      left: cursors.left,
+      right: cursors.right,
+      up: cursors.up,
+      down: cursors.down,
+      speed: 0.5
+  };
+  controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
+
+  shiftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
 }
 
-function update() { }
+function update(time,delta) {
+  controls.update(delta);
+
+  var worldPoint = this.input.activePointer.positionToCamera(this.cameras.main);
+
+  // Rounds down to nearest tile
+  var pointerTile = map.worldToTileXY(worldPoint.x,worldPoint.y,true);
+  // Snap to tile coordinates, but in world space
+  markerP = map.tileToWorldXY(pointerTile.x,pointerTile.y);
+
+  marker.x = markerP.x
+  marker.y = markerP.y
+  if (this.input.manager.activePointer.isDown)
+    {
+        if (shiftKey.isDown)
+        {
+          selectedTile = map.getTileAt(pointerTile.x, pointerTile.y)
+          console.log("tile position : ",pointerTile.x, pointerTile.y)
+          console.log(selectedTile)
+        }
+        else if (selectedTile !== undefined)
+        {
+          console.log("trying to put at ", pointerTile.x, pointerTile.y)
+          map.putTileAt(selectedTile, pointerTile.x, pointerTile.y)
+        }
+    }
+ }
+
